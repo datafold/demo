bb768122e840b6d5bae7d46063f54dddfefafc79
Will Sweet <will@datafold.com>
2022-10-17 18:15:02 -0400

    add models

---
+++ models/syncs/sales_sync.sql
@@ -0,0 +1,20 @@
+WITH org_events AS (
+  SELECT
+     *
+  FROM {{ ref('dim_orgs') }}
+  LEFT JOIN {{ source('EVENTS', 'FEATURE_USED') }} USING (org_id)
+  WHERE sub_plan IS NULL 
+)
+
+, final AS (
+    SELECT 
+        DISTINCT ORG_ID
+        , count(*) AS usage
+    FROM org_events
+    WHERE event_timestamp < dateadd('day', 30, created_at)
+        AND event_timestamp > dateadd('day', -21, current_date())
+        AND created_at > dateadd('day', -30, current_date())
+    GROUP BY 1
+)
+
+SELECT * FROM final
