249efc2452b1706289839e6baaaad3a112155727
kylemcnair <40182913+kylemcnair@users.noreply.github.com>
2023-02-24 07:03:34 -0800

    swap sign_up for org_created

--- models/core/dim_orgs.sql
+++ models/core/dim_orgs.sql
@@ -1,9 +1,10 @@
 WITH orgs AS (
     SELECT 
         org_id
-        , MIN(event_timestamp) AS created_at
-    FROM {{ ref('signed_in') }}
-    GROUP BY 1
+        , org_name
+        , employee_range
+        , created_at
+    FROM {{ ref('org_created') }}
 )
 
 , user_count AS (
