09c35d0a75cb7a7a5f6cabd02ef026d1833267ad
kylemcnair <40182913+kylemcnair@users.noreply.github.com>
2023-03-14 11:53:47 -0700

    use dates as keys for fct financials tables

--- models/core/fct_yearly_financials.sql
+++ models/core/fct_yearly_financials.sql
@@ -1,7 +1,8 @@
 select
-    date_trunc('year', date_month) as date_year
+    date_year
+    , date_month
     , sum(cnt_subscribers)         as yearly_subscribers
     , sum(sum_revenue)             as yearly_revenue
     , hash(date_year) as id
 from {{ref('fct_monthly_financials')}}
-group by 1
+group by 1 , 2
