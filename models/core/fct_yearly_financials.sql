e065b80cccdc3be735fdd5a1bd94a7dea4414b3d
Will Sweet <11434284+williebsweet@users.noreply.github.com>
2023-03-14 16:08:51 -0400

    remove date_month

--- models/core/fct_yearly_financials.sql
+++ models/core/fct_yearly_financials.sql
@@ -1,7 +1,7 @@
 select
     date_year
-    , date_month
+    -- , date_month
     , sum(cnt_subscribers)         as yearly_subscribers
     , sum(sum_revenue)             as yearly_revenue
 from {{ref('fct_monthly_financials')}}
-group by 1 , 2
+group by 1
