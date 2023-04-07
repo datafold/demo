b223c65a6d1ac51931f72b5290114870ce780958
kylemcnair <40182913+kylemcnair@users.noreply.github.com>
2023-04-07 10:29:02 -0700

    dim_orgs change

--- models/core/dim_orgs.sql
+++ models/core/dim_orgs.sql
@@ -2,9 +2,10 @@ WITH orgs AS (
 --prod
     SELECT
         org_id
-        , MIN(event_timestamp) AS created_at
-    FROM {{ ref('signed_in') }}
-    GROUP BY 1
+        , org_name
+        , employee_range
+        , created_at
+    FROM {{ ref('org_created') }}
 
 -- --dev
 --    SELECT
