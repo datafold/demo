1327f73979837ac00c2a94fd830f421b7353ce7e
Leo Folsom <leoebfolsom@gmail.com>
2023-04-17 16:08:20 -0700

    fiddle with pricing

--- models/core/dim_orgs.sql
+++ models/core/dim_orgs.sql
@@ -28,7 +28,7 @@ WITH orgs AS (
         org_id
         , event_timestamp AS sub_created_at
         , plan as sub_plan
-        , price as sub_price
+        , price+1 as sub_price
     FROM {{ ref('subscription_created') }}
 )
 
