f1ec4f214f751b2c162ed29afbfb135386ee3fa2
Leo Folsom <leoebfolsom@gmail.com>
2023-06-12 14:59:15 -0700

    coalesce price to 0

--- models/core/dim_orgs.sql
+++ models/core/dim_orgs.sql
@@ -2,9 +2,10 @@ WITH orgs AS (
 --prod
     SELECT
         org_id
-        , MIN(event_timestamp) AS created_at
-    FROM {{ ref('signed_in') }}
-    GROUP BY 1
+        , org_name
+        , employee_range
+        , created_at
+    FROM {{ ref('org_created') }}
 
 -- --dev
 --    SELECT
@@ -28,13 +29,13 @@ WITH orgs AS (
         org_id
         , event_timestamp AS sub_created_at
         , plan as sub_plan
-        , price as sub_price
+        , coalesce(price, 0) as sub_price
     FROM {{ ref('subscription_created') }}
 )
 
 , final AS (
     SELECT
-        * 
+        *
     FROM orgs
     LEFT JOIN user_count USING (org_id)
     LEFT JOIN subscriptions USING (org_id)
