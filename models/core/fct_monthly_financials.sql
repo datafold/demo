e6943845fc5a736bc411cd551c1f58b4d228d064
Jai Toor <jai@datafold.com>
2023-07-18 19:47:52 -0700

    Update price, run CI please

--- models/core/fct_monthly_financials.sql
+++ models/core/fct_monthly_financials.sql
@@ -2,7 +2,8 @@ WITH final AS (
     SELECT 
         date_trunc('month', sub_created_at) as date_month
         , count(distinct org_id) as cnt_subscribers
-        , sum(sub_price) as sum_revenue
+        -- Remove 5 cents from each price for fees
+        , sum(sub_price-.05) as sum_revenue
     FROM {{ ref('dim_orgs') }}
     WHERE sub_created_at is not NULL 
     GROUP BY 1 
