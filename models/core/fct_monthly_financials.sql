788495af1a801675589aadbc33aad6e869295740
Leo Folsom <leoebfolsom@gmail.com>
2023-08-04 12:46:00 -0700

    rename price

--- models/core/fct_monthly_financials.sql
+++ models/core/fct_monthly_financials.sql
@@ -2,7 +2,7 @@ WITH final AS (
     SELECT 
         date_trunc('month', sub_created_at) as date_month
         , count(distinct org_id) as cnt_subscribers
-        , sum(sub_price) as sum_revenue
+        , sum(price) as sum_revenue
     FROM {{ ref('dim_orgs') }}
     WHERE sub_created_at is not NULL 
     GROUP BY 1 
