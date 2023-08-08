460c9b5ae7b51c538a2556792efc6bd10b5cff94
Leo Folsom <leoebfolsom@gmail.com>
2023-08-08 15:17:18 -0700

    change date range

--- models/syncs/sales_sync.sql
+++ models/syncs/sales_sync.sql
@@ -14,7 +14,7 @@ WITH org_events AS (
     WHERE
         -- select orgs created within the last 60 days, with usage within the 30 days
         event_timestamp::date > ('2022-11-01'::date - 30)
-        AND created_at::date > ('2022-11-01'::date - 60)
+        AND created_at::date > ('2022-11-01'::date - 61)
     GROUP BY 1
 )
 
