ba773d2a6ef4368154b5111cd09e8a90c3e9edae
roconno1 <william_r_oconnor@progressive.com>
2023-11-30 12:31:20 -0500

    Change time window to 45 days

--- models/syncs/sales_sync.sql
+++ models/syncs/sales_sync.sql
@@ -3,18 +3,18 @@ WITH org_events AS (
      *
   FROM {{ ref('dim_orgs') }}
   LEFT JOIN {{ ref('feature_used') }} USING (org_id)
-  WHERE sub_plan IS NULL 
+  WHERE sub_plan IS NULL
 )
 
 , final AS (
-    SELECT 
+    SELECT
         DISTINCT ORG_ID
         , count(*) AS usage
     FROM org_events
     WHERE
         -- select orgs created within the last 60 days, with usage within the 30 days
         event_timestamp::date > ('2022-11-01'::date - 30)
-        AND created_at::date > ('2022-11-01'::date - 60)
+        AND created_at::date > ('2022-11-01'::date - 45)
     GROUP BY 1
 )
 
