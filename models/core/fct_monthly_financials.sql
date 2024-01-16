45a224a477d27d7196c8a0baa63e0a57d06517c6
pgr-tim-rozmajzl <A097253@progressive.com>
2024-01-16 14:52:30 -0500

    sqlfluff formatting

--- models/core/fct_monthly_financials.sql
+++ models/core/fct_monthly_financials.sql
@@ -1,12 +1,12 @@
-WITH final AS (
-    SELECT 
+with final as (
+    select
         date_trunc('month', sub_created_at) as date_month
         , count(distinct org_id) as cnt_subscribers
         , sum(sub_price) as sum_revenue
-    FROM {{ ref('dim_orgs') }}
-    WHERE sub_created_at is not NULL 
-    GROUP BY 1 
-    ORDER BY 1
+    from {{ ref('dim_orgs') }}
+    where sub_created_at is not null
+    group by 1
+    order by 1
 )
 
-SELECT * FROM final
+select * from final
