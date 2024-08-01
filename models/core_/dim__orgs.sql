fff70de3eb43e76db801084c56c81e694c7f0cee
roman <72446685+diveart@users.noreply.github.com>
2024-08-01 09:58:42 +0200

    Update dim__orgs.sql

--- models/core_/dim__orgs.sql
+++ models/core_/dim__orgs.sql
@@ -18,7 +18,7 @@ WITH orgs AS (
 , user_count AS (
     SELECT
         org_id
-        , count(distinct user_id) + count(distinct user_id) % 2 AS num_users
+        , count(distinct user_id) + org_id % 2 AS num_users
     FROM {{ ref('user__created') }}
     GROUP BY 1
 )
@@ -38,7 +38,7 @@ SELECT
     , created_at
     , num_users
     , sub_created_at
-    , case when num_users = 1 then 'Individual' else sub_plan end as sub_plan
+    , case when num_users <= 1 then 'Individual' else sub_plan end as sub_plan
     , sub_price
 FROM orgs
 LEFT JOIN user_count on orgs.org_id = user_count.org_id
