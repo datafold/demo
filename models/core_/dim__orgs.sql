cb5c82609e1a1b265f2b1939e900c346cff4aaa4
Leo Folsom <leoebfolsom@gmail.com>
2024-10-22 08:52:46 -0700

    Update dim__orgs.sql

--- models/core_/dim__orgs.sql
+++ models/core_/dim__orgs.sql
@@ -1,18 +1,18 @@
 WITH orgs AS (
 --prod
-    SELECT
-        org_id
-        , MIN(event_timestamp) AS created_at
-    FROM {{ ref('signed__in') }}
-    GROUP BY 1
-
--- --dev
 --    SELECT
---         org_id
---         , org_name
---         , employee_range
---         , created_at
---     FROM {{ ref('org__created') }}
+--        org_id
+--        , MIN(event_timestamp) AS created_at
+--    FROM {{ ref('signed__in') }}
+--    GROUP BY 1
+
+ --dev
+    SELECT
+         org_id
+         , org_name
+         , employee_range
+         , created_at
+     FROM {{ ref('org__created') }}
 )
 
 , user_count AS (
@@ -34,12 +34,12 @@ WITH orgs AS (
 
 
 SELECT
-    orgs.org_id
+    org_id
     , created_at
-    , num_users
+    , case when num_users > 4 then 4 else num_users end as num_users
     , sub_created_at
-    , sub_plan
-    , sub_price
+    , case when num_users = 1 then 'Individual' else sub_plan end as sub_plan
+    , case when sub_price = 99 then 100 else sub_price end as sub_price
 FROM orgs
 LEFT JOIN user_count on orgs.org_id = user_count.org_id
 LEFT JOIN subscriptions on orgs.org_id = subscriptions.org_id
