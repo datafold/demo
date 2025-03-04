500c129556e907817a32e756ee8f98cb7559ec85
roman <roman@datafold.com>
2025-03-04 11:12:08 +0100

    demo model

---
+++ models/informatica/monthly_revenue.sql
@@ -0,0 +1,18 @@
+{{
+  config(
+    materialized='table'
+  )
+}}
+
+WITH subscription_revenue AS (
+    SELECT 
+        SUBSTR(TO_CHAR((EVENT_TIMESTAMP::TIMESTAMPNTZ)::TIMESTAMPNTZ, 'YYYY-MM'), 0, 10) AS month,
+        SUM(PRICE::DOUBLE)::NUMBER(10,0) AS revenue
+    FROM {{ ref('subscription__created') }}
+    GROUP BY 1
+)
+
+SELECT 
+    revenue::NUMBER(18,0),
+    month
+FROM subscription_revenue 
