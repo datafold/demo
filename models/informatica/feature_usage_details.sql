3e1f80f9ba2e173798b6c129d00eface1fc9a5a6
roman <roman@datafold.com>
2025-03-07 18:16:56 +0100

    hide informatica models

--- models/informatica/feature_usage_details.sql
+++ models/informatica/feature_usage_details.sql
@@ -1,35 +1,37 @@
-{{
-  config(
-    materialized='table'
-  )
-}}
+select 1 as col
 
-WITH org_data AS (
-    SELECT 
-        ORG_ID::DOUBLE AS ORG_ID,
-        ORG_NAME,
-        DOMAIN,
-        EMPLOYEE_RANGE,
-        CREATED_AT::TIMESTAMPNTZ AS CREATED_AT
-    FROM {{ ref('org__created') }}
-),
+-- {{
+--   config(
+--     materialized='table'
+--   )
+-- }}
 
-feature_usage AS (
-    SELECT 
-        ORG_ID::DOUBLE AS ORG_ID,
-        EVENT_TIMESTAMP::TIMESTAMPNTZ AS EVENT_TIMESTAMP,
-        ACTIVITY
-    FROM {{ ref('feature__used') }}
-)
+-- WITH org_data AS (
+--     SELECT 
+--         ORG_ID::DOUBLE AS ORG_ID,
+--         ORG_NAME,
+--         DOMAIN,
+--         EMPLOYEE_RANGE,
+--         CREATED_AT::TIMESTAMPNTZ AS CREATED_AT
+--     FROM {{ ref('org__created') }}
+-- ),
 
-SELECT 
-    f.ORG_ID::NUMBER(38,0) AS "FU_ORG_ID",
-    f.EVENT_TIMESTAMP::TIMESTAMPNTZ AS "FU_EVENT_TIMESTAMP",
-    f.ACTIVITY::VARCHAR(16777216) AS "FU_ACTIVITY",
-    o.ORG_ID::NUMBER(38,0) AS "OC_ORG_ID",
-    o.ORG_NAME::VARCHAR(16777216) AS "OC_ORG_NAME",
-    o.DOMAIN::VARCHAR(16777216) AS "OC_DOMAIN",
-    o.EMPLOYEE_RANGE::VARCHAR(16777216) AS "OC_EMPLOYEE_RANGE",
-    o.CREATED_AT::TIMESTAMPNTZ AS "OC_CREATED_AT"
-FROM feature_usage f
-JOIN org_data o ON f.ORG_ID = o.ORG_ID 
+-- feature_usage AS (
+--     SELECT 
+--         ORG_ID::DOUBLE AS ORG_ID,
+--         EVENT_TIMESTAMP::TIMESTAMPNTZ AS EVENT_TIMESTAMP,
+--         ACTIVITY
+--     FROM {{ ref('feature__used') }}
+-- )
+
+-- SELECT 
+--     f.ORG_ID::NUMBER(38,0) AS "FU_ORG_ID",
+--     f.EVENT_TIMESTAMP::TIMESTAMPNTZ AS "FU_EVENT_TIMESTAMP",
+--     f.ACTIVITY::VARCHAR(16777216) AS "FU_ACTIVITY",
+--     o.ORG_ID::NUMBER(38,0) AS "OC_ORG_ID",
+--     o.ORG_NAME::VARCHAR(16777216) AS "OC_ORG_NAME",
+--     o.DOMAIN::VARCHAR(16777216) AS "OC_DOMAIN",
+--     o.EMPLOYEE_RANGE::VARCHAR(16777216) AS "OC_EMPLOYEE_RANGE",
+--     o.CREATED_AT::TIMESTAMPNTZ AS "OC_CREATED_AT"
+-- FROM feature_usage f
+-- JOIN org_data o ON f.ORG_ID = o.ORG_ID 
--- models/informatica/monthly_revenue.sql
+++ models/informatica/monthly_revenue.sql
@@ -1,18 +1,20 @@
-{{
-  config(
-    materialized='table'
-  )
-}}
+select 1 as col
 
-WITH subscription_revenue AS (
-    SELECT 
-        SUBSTR(TO_CHAR((EVENT_TIMESTAMP::TIMESTAMPNTZ)::TIMESTAMPNTZ, 'YYYY-MM'), 0, 10) AS month,
-        SUM(PRICE::DOUBLE)::NUMBER(10,0) AS revenue
-    FROM {{ ref('subscription__created') }}
-    GROUP BY 1
-)
+-- {{
+--   config(
+--     materialized='table'
+--   )
+-- }}
 
-SELECT 
-    revenue::NUMBER(10,0) AS revenue,
-    month::VARCHAR(10) AS month
-FROM subscription_revenue 
+-- WITH subscription_revenue AS (
+--     SELECT 
+--         SUBSTR(TO_CHAR((EVENT_TIMESTAMP::TIMESTAMPNTZ)::TIMESTAMPNTZ, 'YYYY-MM'), 0, 10) AS month,
+--         SUM(PRICE::DOUBLE)::NUMBER(10,0) AS revenue
+--     FROM {{ ref('subscription__created') }}
+--     GROUP BY 1
+-- )
+
+-- SELECT 
+--     revenue::NUMBER(10,0) AS revenue,
+--     month::VARCHAR(10) AS month
+-- FROM subscription_revenue 
