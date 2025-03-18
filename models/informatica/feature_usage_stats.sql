ff1ba1727355776fa95931577b0e00aa8e07732a
roman <roman@datafold.com>
2025-03-18 21:46:05 +0100

    informatica upd

--- models/informatica/feature_usage_stats.sql
+++ models/informatica/feature_usage_stats.sql
@@ -4,29 +4,27 @@
   )
 }}
 
-select 1 as col
+WITH subscription_data AS (
+    SELECT 
+        ORG_ID::DOUBLE AS ORG_ID,
+        PLAN
+    FROM {{ ref('subscription__created') }}
+),
 
--- WITH subscription_data AS (
---     SELECT 
---         ORG_ID::DOUBLE AS ORG_ID,
---         PLAN
---     FROM {{ ref('subscription__created') }}
--- ),
+feature_usage AS (
+    SELECT 
+        ORG_ID::DOUBLE AS ORG_ID,
+        ACTIVITY,
+        TO_CHAR((EVENT_TIMESTAMP::TIMESTAMPNTZ)::TIMESTAMPNTZ, 'MM')::NUMBER(10,0) AS month_num
+    FROM {{ ref('feature__used') }}
+    WHERE TO_CHAR((EVENT_TIMESTAMP::TIMESTAMPNTZ)::TIMESTAMPNTZ, 'MM')::NUMBER(10,0) = 1
+)
 
--- feature_usage AS (
---     SELECT 
---         ORG_ID::DOUBLE AS ORG_ID,
---         ACTIVITY,
---         TO_CHAR((EVENT_TIMESTAMP::TIMESTAMPNTZ)::TIMESTAMPNTZ, 'MM')::NUMBER(10,0) AS month_num
---     FROM {{ ref('feature__used') }}
---     WHERE TO_CHAR((EVENT_TIMESTAMP::TIMESTAMPNTZ)::TIMESTAMPNTZ, 'MM')::NUMBER(10,0) = 1
--- )
-
--- SELECT 
---     COUNT(f.ORG_ID)::NUMBER(10,0) AS "org_cnt",
---     ANY_VALUE(f.ORG_ID)::NUMBER(38,0) AS "fu_ORG_ID",
---     f.ACTIVITY::VARCHAR(16777216) AS "fu_ACTIVITY",
---     s.PLAN::VARCHAR(16777216) AS "sc_PLAN"
--- FROM subscription_data s
--- JOIN feature_usage f ON f.ORG_ID = s.ORG_ID
--- GROUP BY s.PLAN, f.ACTIVITY 
+SELECT 
+    COUNT(f.ORG_ID)::NUMBER(10,0) AS "org_cnt",
+    ANY_VALUE(f.ORG_ID)::NUMBER(38,0) AS "fu_ORG_ID",
+    f.ACTIVITY::VARCHAR(16777216) AS "fu_ACTIVITY",
+    s.PLAN::VARCHAR(16777216) AS "sc_PLAN"
+FROM subscription_data s
+JOIN feature_usage f ON f.ORG_ID = s.ORG_ID
+GROUP BY s.PLAN, f.ACTIVITY 
--- models/informatica/monthly_revenue.sql
+++ models/informatica/monthly_revenue.sql
@@ -4,17 +4,15 @@
   )
 }}
 
-select 1 as col
+WITH subscription_revenue AS (
+    SELECT 
+        SUBSTR(TO_CHAR((EVENT_TIMESTAMP::TIMESTAMPNTZ)::TIMESTAMPNTZ, 'YYYY-MM'), 0, 10) AS month,
+        SUM(PRICE::DOUBLE)::NUMBER(10,0) AS revenue
+    FROM {{ ref('subscription__created') }}
+    GROUP BY 1
+)
 
--- WITH subscription_revenue AS (
---     SELECT 
---         SUBSTR(TO_CHAR((EVENT_TIMESTAMP::TIMESTAMPNTZ)::TIMESTAMPNTZ, 'YYYY-MM'), 0, 10) AS month,
---         SUM(PRICE::DOUBLE)::NUMBER(10,0) AS revenue
---     FROM {{ ref('subscription__created') }}
---     GROUP BY 1
--- )
-
--- SELECT 
---     revenue::NUMBER(10,0) AS revenue,
---     month::VARCHAR(10) AS month
--- FROM subscription_revenue 
+SELECT 
+    revenue::NUMBER(10,0) AS revenue,
+    month::VARCHAR(10) AS month
+FROM subscription_revenue 
